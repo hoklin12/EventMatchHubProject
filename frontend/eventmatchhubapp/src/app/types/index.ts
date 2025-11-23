c87be5ae3a2e13228744adaf78a2311005bb4e26
povrajanapen <povrajanapen@gmail.com>
2025-11-24 00:39:28 +0700

    feature: login and signup pages

--- frontend/eventmatchhubapp/src/app/types/index.ts
+++ frontend/eventmatchhubapp/src/app/types/index.ts
@@ -1,14 +1,20 @@
 // app/types/index.ts
 
 export interface Event {
-  id: number;
-  title: string;
-  category: string;
-  date: string;
-  location: string;
-  attendees: number;
-  image: string;
-  featured?: boolean;
+  id: number
+  title: string
+  category: string
+  date: string
+  time: string
+  location: string
+  organizer: string
+  attendees: number
+  maxAttendees: number
+  price: number
+  rating: number
+  image: string
+  featured?: boolean
+  tags: string[]
 }
 
 export interface Category {
